824d59ec27c3ad42569bc3840a0ef7a1927f23b1
Raphael ASTIER <r.astier.dev@gmail.com>
2022-06-07 15:21:48 -0400

    Add cloudinary support for production

---
+++ config/plugins.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => {
+    if (env('NODE_ENV') !== 'development') {
+        return {
+            upload: {
+                config: {
+                    provider: 'cloudinary',
+                    providerOptions: {
+                        cloud_name: env('CLOUDINARY_NAME'),
+                        api_key: env('CLOUDINARY_KEY'),
+                        api_secret: env('CLOUDINARY_SECRET'),
+                    },
+                    actionOptions: {
+                        upload: {},
+                        delete: {},
+                    },
+                },
+            }
+        }
+    }
+};
