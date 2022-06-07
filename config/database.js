21ca5df4eb59d3767eda037e7730cde3e7c0019c
Raphael ASTIER <r.astier.dev@gmail.com>
2022-06-07 14:56:13 -0400

    Add postgres configuration for production

--- config/database.js
+++ config/database.js
@@ -1,11 +1,34 @@
 const path = require('path');
+const parsePG = require('pg-connection-string').parse;
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+module.exports = ({ env }) => {
+    if (env('NODE_ENV') === 'development') {
+        return {
+            connection: {
+                client: 'sqlite',
+                connection: {
+                    filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+                },
+                useNullAsDefault: true,
+            },
+        }
+    }
+
+    const config = parsePG(env('DATABASE_URL'));
+    return {
+        connection: {
+            client: 'postgres',
+            connection: {
+                host: config.host,
+                port: config.port,
+                database: config.database,
+                user: config.user,
+                password: config.password,
+                ssl: {
+                    rejectUnauthorized: false
+                }
+            }
+        }
+    }
+};
+    
